48ba00d89d34b99a4f2155394b1767ad0bb60a97
mrvitalya <sveta@Sveta-Air.local>
2013-08-30 23:11:29 +0300

    Initial Commit

---
+++ Address book/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Address book
+//
+//  Created by Sveta on 30.08.13.
+//  Copyright (c) 2013 Sveta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
