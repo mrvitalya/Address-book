f3711651945e6939164f0bb23c7c03bc3e8e6b4c
mrvitalya <sveta@Sveta-Air.local>
2013-08-30 23:38:13 +0300

    Initial commit

---
+++ Address book/AddressViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddressViewController.h
+//  Address book
+//
+//  Created by Sveta on 30.08.13.
+//  Copyright (c) 2013 Sveta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddressViewController : UITableViewController
+
+@end
