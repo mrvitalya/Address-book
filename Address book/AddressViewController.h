e9c99ac652343aaa27c097d089578769a299062c
mrvitalya <sveta@217-237-203-46.pool.ukrtel.net>
2013-09-07 04:37:49 +0300

    Revert "finaly version"
    This reverts commit fa3134772b58c8620ea31c7187ef1429a67c6afa.

--- Address book/AddressViewController.h
+++ Address book/AddressViewController.h
@@ -1,5 +1,5 @@
 //
-//  AppDelegate.h
+//  AddressViewController.h
 //  Address book
 //
 //  Created by Sveta on 30.08.13.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
+@interface AddressViewController : UITableViewController
 
 @end
